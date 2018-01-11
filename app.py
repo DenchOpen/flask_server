2f080dd00babd900fa6200e0e3f30603f00b6332
Dench <denchopen@gmail.com>
2018-01-12 00:59:59 +0800

    login and register html by bootcss

--- app.py
+++ app.py
@@ -18,7 +18,18 @@ with app.app_context():
 
 @app.route('/')
 def index():
+    print('index refresh')
     return render_template('index.html')
+
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register/')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
