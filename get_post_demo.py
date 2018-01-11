7ba77d48e57830637b6d633c1ed2009034e41e87
Dench <denchopen@gmail.com>
2018-01-11 16:33:48 +0800

    get post form before_request and g

--- get_post_demo.py
+++ get_post_demo.py
@@ -2,9 +2,11 @@
 get & post
 """
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, g
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
 
 
 @app.route('/')
@@ -23,9 +25,36 @@ def search():
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return 'login'
+        return render_template('login.html')
     else:
-        return 'post request'
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if password == '111111':
+            session['username'] = username
+            return 'success'
+        else:
+            session.clear()
+            return 'username or password error.'
+
+
+@app.route('/edit/')
+def edit_user():
+    if g.get('user'):
+        return 'hello ' + g.user
+    else:
+        return redirect(url_for('login'))
+
+
+@app.before_request
+def bf_request():
+    username = session.get('username')
+    print("username:", username)
+    if username is not None:
+        if g.get('user') or g.get('user') != username:
+            g.user = username
+    else:
+        if g.get('user'):
+            g.pop('user')
 
 
 if __name__ == '__main__':
